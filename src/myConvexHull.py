a206a45201f5960ec00fa407f72ed2e14ffc3078
eiffelaqila <16520324@std.stei.itb.ac.id>
2022-02-28 13:50:17 +0700

    revisi angle

--- src/myConvexHull.py
+++ src/myConvexHull.py
@@ -52,7 +52,7 @@ def angle(p1, p2, p3):
     """
     Mengembalikan besar sudut p1-p2-p3
     """
-    ang = math.degrees(math.atan2(p3[1]-p2[1], p3[0]-p2[0]) - math.atan2(p1[1]-p2[1], p1[0]-p2[0]))
+    ang = math.degrees(math.atan2(p3[1]-p1[1], p3[0]-p1[0]) - math.atan2(p2[1]-p1[1], p2[0]-p1[0]))
     if ang < 0:
         return ang+360
     else:
